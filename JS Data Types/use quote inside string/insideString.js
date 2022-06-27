93c4ea452adac7ab38f2fd0619da0361b65efd8f
roze28 <firoze.hossain01@yahoo.com>
2022-06-27 18:03:18 +0600

    use quote inside string

---
+++ JS Data Types/use quote inside string/insideString.js
@@ -0,0 +1,5 @@
+//can use quote inside string
+let firstName = "Md. 'Firoze'";
+let lastName = "'Hossain'";
+let fullName = '"Md. Firoze Hossain"';
+console.log(firstName + " " + lastName + " " + fullName);
