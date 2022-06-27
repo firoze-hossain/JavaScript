62c79b3d781e1fe9757bf9d10155ea6e0930ac09
roze28 <firoze.hossain01@yahoo.com>
2022-06-27 19:40:39 +0600

    typeof operator return the type of variable or expression

---
+++ JS Data Types/typeof operator/typeofOperator.js
@@ -0,0 +1,5 @@
+//typeof operator return the type of variable or expression
+let x = "";
+let firstName = "Md. Firoze";
+let lastName = "Hossain";
+console.log(typeof x + "\n" + typeof firstName + "\n" + typeof lastName);
