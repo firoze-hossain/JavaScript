fb59968df5151a76c2b3c7f30bd7f092885dbe36
roze28 <firoze.hossain01@yahoo.com>
2022-06-27 19:18:42 +0600

    javascript objects are written with {} braces and written with name:value pairs

---
+++ JS Data Types/objects/objects.js
@@ -0,0 +1,14 @@
+// javascript objects are written with {} braces and written with name:value pairs
+const person = {
+  name: "Md. Firoze Hossain",
+  age: 28,
+  contact: "01765002428",
+};
+console.log(
+  "Full Details of person:\n" +
+    person.name +
+    "\n" +
+    person.age +
+    "\n" +
+    person.contact
+);
