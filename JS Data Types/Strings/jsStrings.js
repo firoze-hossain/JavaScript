b2c99682d447b42537be59227708a29c6d9eb129
roze28 <firoze.hossain01@yahoo.com>
2022-06-27 17:59:11 +0600

    string or text string are series of characters

---
+++ JS Data Types/Strings/jsStrings.js
@@ -0,0 +1,5 @@
+//string or text string are series of characters
+//use single double quotes to declare string
+let carName1 = "Volvo";
+let carName2 = "BMW";
+console.log("Cars Name Are: " + carName1 + " " + carName2);
