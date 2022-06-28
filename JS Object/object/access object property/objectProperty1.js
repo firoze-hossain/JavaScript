998bd6f1fbb316e92125d5a0086949433f0a18d2
roze28 <firoze.hossain01@yahoo.com>
2022-06-28 15:54:06 +0600

    access object property using objectName.propertyName

---
+++ JS Object/object/access object property/objectProperty1.js
@@ -0,0 +1,3 @@
+//access object property using objectName.propertyName
+const person = { name: "Firoze", age: 28, height: 5.11 };
+console.log(person.name + "\n" + person.age + "\n" + person.height);
