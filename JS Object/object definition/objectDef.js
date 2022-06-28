4e070d60ba64c16a818df8987e1d5bf1c9cf9297
roze28 <firoze.hossain01@yahoo.com>
2022-06-28 14:22:50 +0600

    define javascript oject with object literal

---
+++ JS Object/object definition/objectDef.js
@@ -0,0 +1,14 @@
+//define javascript oject with object literal
+const person = {
+  name: "Md. Firoze hossain",
+  profession: "Java Developer",
+  company: "Leads",
+};
+console.log(
+  person.name +
+    " is " +
+    person.profession +
+    " at " +
+    person.company +
+    " Corporation Ltd."
+);
