b1334819ceec48dfdaa8c9c08a2ebf1f49eab3f6
roze28 <firoze.hossain01@yahoo.com>
2022-06-28 16:02:02 +0600

    access object property using objectName["propertyName"]

---
+++ JS Object/object/access object property/objectProperty2.js
@@ -0,0 +1,5 @@
+//access object property using objectName["propertyName"]
+const car = { name: "BMW", color: "Red", price: 100000 };
+console.log(
+  "Car Name: " + car["name"] + ",Color: " + car["color"] + ",Price: " + car["price"]
+);
