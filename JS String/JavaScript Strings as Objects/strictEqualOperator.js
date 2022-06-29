bcd9a95f17a419054b7b1ff25bd52a1bb9a7232f
roze28 <firoze.hossain01@yahoo.com>
2022-06-29 13:33:36 +0600

    do not create string as object

---
+++ JS String/JavaScript Strings as Objects/strictEqualOperator.js
@@ -0,0 +1,6 @@
+//when using === it produces false result
+// do not create string as object
+//it produces unexpected result
+let x = "Firoze";
+let y = new String("Firoze");
+console.log(x === y);
