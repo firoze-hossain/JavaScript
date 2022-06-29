17ae7301de3119798e20f2f5b59407c7ac98d55e
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 01:13:42 +0600

    if second parameter has been omitted,slice(-x) with negative parameter will extract the left string

---
+++ JavaScript Strings Method/String slice/omitWithNegativeParameter.js
@@ -0,0 +1,4 @@
+// if second parameter has been omitted,slice(-x) with negative parameter will extract the left string
+let fruit = "Mango, Banana, Apple";
+let leftFruit = fruit.slice(-13);
+console.log(leftFruit);
