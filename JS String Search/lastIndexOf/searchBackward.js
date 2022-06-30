3a1761782f3e16d5886a6fb1c996df6ec3a96d38
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 23:26:28 +0600

    lastIndexOf() method search backward

---
+++ JS String Search/lastIndexOf/searchBackward.js
@@ -0,0 +1,4 @@
+// lastIndexOf() method search backward
+//if second parameter is 15,it starts search from 15 and searches to beginning of the string
+let text = "Hello Firoze. How are you firoze?";
+console.log(text.lastIndexOf("Firoze", 15));
