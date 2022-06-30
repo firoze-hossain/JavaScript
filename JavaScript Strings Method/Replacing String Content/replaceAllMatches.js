739066af49a50bbc09132ce08b675548f85b717f
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 14:55:04 +0600

    to replace all matches use regular expression with g flag

---
+++ JavaScript Strings Method/Replacing String Content/replaceAllMatches.js
@@ -0,0 +1,4 @@
+//to replace all matches use regular expression with g flag
+let text = "Visit Microsoft. Let know more about Microsoft";
+let updatedText = text.replace(/Microsoft/g, "Google");
+console.log(updatedText);
