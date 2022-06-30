64d3a1a42aba9f33175ab4fe58e9b95e3fbdd2ca
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 14:38:45 +0600

    replace method replaces the string with only first match

---
+++ JavaScript Strings Method/Replacing String Content/replaceFirstMatch.js
@@ -0,0 +1,4 @@
+//replace method replaces the string with only first match
+let info = "Microsoft is a software Company. Microsoft is great company.";
+let upInfo = info.replace("Microsoft", "Google");
+console.log(upInfo);
