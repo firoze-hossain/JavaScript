f4861c030402a87ccec62ed40a81fa2d455ce230
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 15:10:57 +0600

    js trim() method replaces white space from both side

---
+++ JavaScript Strings Method/trim method/stringTrim.js
@@ -0,0 +1,5 @@
+//js trim() method replaces white space from both side
+let x = "   Hello Firoze.   ";
+let y = "   How are You?   ";
+let finalResult = x.trim() + " " + y.trim();
+console.log(finalResult);
