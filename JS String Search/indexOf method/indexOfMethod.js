68eb68aafde440b1521e3bbb63cf57e51ab2602e
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 20:41:08 +0600

    indexOf() method returns the index of first occurance of a specified text in string

---
+++ JS String Search/indexOf method/indexOfMethod.js
@@ -0,0 +1,3 @@
+// indexOf() method returns the index of first occurance of a specified text in string
+let str = "Please locate your 'locate' postion in Leads Coporation Ltd.";
+console.log(str.indexOf("locate"));
