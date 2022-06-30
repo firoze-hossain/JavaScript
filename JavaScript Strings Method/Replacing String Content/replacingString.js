32010ee068171d50f07e51c773761163c5168ba2
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 14:33:41 +0600

    replace() method replace a string value with another string value

---
+++ JavaScript Strings Method/Replacing String Content/replacingString.js
@@ -0,0 +1,4 @@
+//replace() method replace a string value with another string value
+let car = "BMW, Vlovo, Maruti";
+let replacedString = car.replace("Maruti", "Huandai");
+console.log(replacedString);
