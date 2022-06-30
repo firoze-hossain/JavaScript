d70e7bee289735de25d95fff8fa75e3b66888dd5
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 23:10:39 +0600

    indexOf() method accepts second parameter as starting postion for search

---
+++ JS String Search/indexOf method/secondParameter.js
@@ -0,0 +1,3 @@
+//indexOf() method accepts second parameter as starting postion for search
+let info = "Firoze is Java Developer. Firoze Hossain is Spring Boot Developer.";
+console.log(info.indexOf("Firoze", 20));
