b01d16684f17e73cd0d0336fbc94cc2abea2871c
roze28 <firoze.hossain01@yahoo.com>
2022-07-01 01:09:10 +0600

    match() method returns a string for a match against regular expression, match return array of object

---
+++ JS String Search/string match method/matchMethod.js
@@ -0,0 +1,3 @@
+// match() method returns a string for a match against regular expression, match return array of object
+let text = "Time and Tide wait for none. Ting Ting Tong";
+console.log(text.match(/Ti/g));
