69bd0819339327f9407fbf19435387993244e8d6
roze28 <firoze.hossain01@yahoo.com>
2022-06-30 15:46:47 +0600

    string split() method converts string into array

---
+++ JavaScript Strings Method/converting string an array/stringSplit.js
@@ -0,0 +1,8 @@
+// string split() method converts string into array
+// (", ") pipe by comma
+//(" ") pipe by space
+//("|") split on pipe
+
+let x = "a,b,c,d,e,f";
+const myArray = x.split(",");
+console.log(myArray[1]);
