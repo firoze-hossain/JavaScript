dafb91b5a137fc6114450d9747ff6c2af61cd6f0
roze28 <firoze.hossain01@yahoo.com>
2022-07-01 15:41:23 +0600

    startsWith(searchValue,start) does the search with a starting position

---
+++ JS String Search/string startsWith/searchWithPosition.js
@@ -0,0 +1,5 @@
+// startsWith(searchValue,start) does the search with a starting position
+let text = "Hello world. Welcome to JS world.";
+console.log(text.startsWith("world"));
+console.log(text.startsWith("world", 6));
+console.log(text.startsWith("world", 5));
