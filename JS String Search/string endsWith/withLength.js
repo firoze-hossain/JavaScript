29736bf8c1fc34b55f713eb59a36320dec1e9679
roze28 <firoze.hossain01@yahoo.com>
2022-07-01 15:58:03 +0600

    endsWith(searchValue,length) returns the value if specified length is the end of searched value

---
+++ JS String Search/string endsWith/withLength.js
@@ -0,0 +1,3 @@
+//endsWith(searchValue,length) returns the value if specified length is the end of searched value
+let text = "Hello Firoze. How are you?";
+console.log(text.endsWith("Firoze", 12));
