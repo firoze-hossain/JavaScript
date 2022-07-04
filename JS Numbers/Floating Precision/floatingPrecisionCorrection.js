fd4cdcff28cba203e902e39bcc27fadf7a49cafb
roze28 <firoze.hossain01@yahoo.com>
2022-07-05 00:34:16 +0600

    To solve floating point arithmetic it helps to multiply and divide

---
+++ JS Numbers/Floating Precision/floatingPrecisionCorrection.js
@@ -0,0 +1,6 @@
+// To solve floating point arithmetic it helps to multiply and divide
+let x = 0.2 + 0.1;
+console.log("0.2+0.1 = " + x);
+
+let y = (0.2 * 10 + 0.1 * 10) / 10;
+console.log("(0.2*10+0.1*10)/10 = " + y);
