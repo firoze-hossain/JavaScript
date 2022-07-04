3f7faf2b24d4f92259ad7eab073827f529d21996
roze28 <firoze.hossain01@yahoo.com>
2022-07-04 23:51:03 +0600

    extra large or small number can be written with scientific notation

---
+++ JS Numbers/Extra Large or Extra Small Number/exLargeOrSmall.js
@@ -0,0 +1,5 @@
+//extra large or small number can be written with scientific notation
+let exLarge = 123e5;
+let exSmall = 123e-5;
+console.log("Extra Large Number = " + exLarge);
+console.log("Extra Small Number = " + exSmall);
