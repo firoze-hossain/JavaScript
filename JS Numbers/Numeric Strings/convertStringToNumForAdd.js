e22046f6b00a4caa90fe143350823831f9f548da
roze28 <firoze.hossain01@yahoo.com>
2022-07-20 17:52:24 +0600

    js will not be success for converting string to number when addition

---
+++ JS Numbers/Numeric Strings/convertStringToNumForAdd.js
@@ -0,0 +1,5 @@
+//js will not be success for converting string to number when addition
+let firstString = "100";
+let secondString = "10";
+let result = firstString + secondString;
+console.log(result);
