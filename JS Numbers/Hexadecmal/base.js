ce4d8548936e19095326982beb0ca9b43951a0cd
roze28 <firoze.hossain01@yahoo.com>
2022-07-20 20:32:02 +0600

    by default js displays number as deccimal

---
+++ JS Numbers/Hexadecmal/base.js
@@ -0,0 +1,16 @@
+//by default js displays number as deccimal
+let num = 32;
+let twoBase = num.toString(2);
+console.log(twoBase);
+
+let tenBase = num.toString(10);
+console.log(tenBase);
+
+let twelveBase = num.toString(12);
+console.log(twelveBase);
+
+let sixteenBase = num.toString(16);
+console.log(sixteenBase);
+
+let eightBase = num.toString(8);
+console.log(eightBase);
