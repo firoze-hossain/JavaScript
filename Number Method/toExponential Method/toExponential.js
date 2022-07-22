60247fbf681007865435d7a09138d661bd6a0222
roze28 <firoze.hossain01@yahoo.com>
2022-07-22 09:52:20 +0600

    toExponential() method returns string with number arounded and written with exponential notation

---
+++ Number Method/toExponential Method/toExponential.js
@@ -0,0 +1,5 @@
+//toExponential() method returns string with number arounded and written with exponential notation
+let x = 9.56;
+console.log(
+  x.toExponential() + "\n" + x.toExponential(2) + "\n" + x.toExponential(5)
+);
