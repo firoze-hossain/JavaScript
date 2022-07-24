6d6614c7b89d260cc614bf42f10e795d5984e9e6
roze28 <firoze.hossain01@yahoo.com>
2022-07-24 20:36:13 +0600

    parseInt() method parses string and returns the whole number number

---
+++ Number Method/parseInt Method/parseInt.js
@@ -0,0 +1,14 @@
+//parseInt() method parses string and returns the whole number number
+console.log(
+  parseInt("-10") +
+    "\n" +
+    parseInt("10") +
+    "\n" +
+    parseInt("10.55") +
+    "\n" +
+    parseInt("10 16") +
+    "\n" +
+    parseInt("55 Kg") +
+    "\n" +
+    parseInt("kg 55")
+);
