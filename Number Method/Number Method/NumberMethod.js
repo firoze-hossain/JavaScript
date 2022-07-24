296db77e29b631ba8569f2aa4510175f87dcec69
roze28 <firoze.hossain01@yahoo.com>
2022-07-24 18:44:58 +0600

    Number() method can be used to convert javaScript variables to numbers

---
+++ Number Method/Number Method/NumberMethod.js
@@ -0,0 +1,12 @@
+//Number() method can be used to convert javaScript variables to numbers
+console.log(
+  Number(true) +
+    "\n" +
+    Number(false) +
+    "\n" +
+    Number("123") +
+    "\n" +
+    Number("Firoze") +
+    "\n" +
+    Number("10 20")
+);
