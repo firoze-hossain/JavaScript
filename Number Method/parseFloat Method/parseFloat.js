81b856ef76ad1b3c27146889295a0e8c479a3d27
roze28 <firoze.hossain01@yahoo.com>
2022-07-24 20:43:15 +0600

    parseFloat() parses string and returns numbers

---
+++ Number Method/parseFloat Method/parseFloat.js
@@ -0,0 +1,12 @@
+//parseFloat() parses string and returns numbers
+console.log(
+  parseFloat("10") +
+    "\n" +
+    parseFloat("10.22") +
+    "\n" +
+    parseFloat("10 22") +
+    "\n" +
+    parseFloat("kg 10.2") +
+    "\n" +
+    parseFloat("10.2 kg")
+);
