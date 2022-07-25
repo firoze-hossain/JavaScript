c320f2ad13d8d660d2730f19af909653061afffa
roze28 <firoze.hossain01@yahoo.com>
2022-07-25 22:43:26 +0600

    one way to loop through an array using for loop

---
+++ Array/Looping Array Element/loopingArrayElement.js
@@ -0,0 +1,6 @@
+//one way to loop through an array using for loop
+const fruits = ["Banana", "Apple", "Mango", "Guava", "Jackfruit", "Orange"];
+console.log("Fruits Name: ");
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
