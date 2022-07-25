a8dd4259e3792a59dbba5b325c7ba5b70d14c21d
roze28 <firoze.hossain01@yahoo.com>
2022-07-25 08:11:22 +0600

    trying to do arithmetic with non-numeric will return NaN

---
+++ Number Method/NaN/jsNaN.js
@@ -0,0 +1,7 @@
+//trying to do arithmetic with non-numeric will return NaN
+
+let x = Number.NaN;
+console.log(x);
+
+let divisionValue = 100 / "Roze";
+console.log('100/"Roze" = ' + divisionValue);
