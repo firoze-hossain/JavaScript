4c9bed0b8163fde78fc1fe6d080c1963851f11b4
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 07:49:51 +0600

    adding new element to an array is to used push() method

---
+++ Array/adding array element/addNewElement.js
@@ -0,0 +1,5 @@
+//adding new element to an array is to used push() method
+const bikes = ["Yamaha", "Bajaj", "Royal Enfield"];
+console.log("Before new Bike add: " + bikes);
+bikes.push("Sujuki");
+console.log("After new bike add: " + bikes);
