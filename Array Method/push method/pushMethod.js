e27421d001da4f8eafbeb92d284b8100200f2e25
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 15:06:35 +0600

    push() method adds a new element to an array to the end

---
+++ Array Method/push method/pushMethod.js
@@ -0,0 +1,5 @@
+//push() method adds a new element to an array to the end
+const bikes = ["Yamaha", "Bajaj", "Sujuki", "Pulsar"];
+console.log("Before adding: " + bikes);
+bikes.push("Royal Enfield");
+console.log("After adding: " + bikes);
