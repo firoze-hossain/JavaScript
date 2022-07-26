00999b8d40ebf5c21231227c8f9d7547edf36329
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 09:11:30 +0600

    By using of named index js method and properties will produce incorrect result

---
+++ Array/associative arrays/incorrectResult.js
@@ -0,0 +1,6 @@
+//By using of named index js method and properties will produce incorrect result
+const person = [];
+person["firstName"] = "Md. Firoze";
+person["lastName"] = "Hossain";
+person["Age"] = 27;
+console.log(person[0]);
