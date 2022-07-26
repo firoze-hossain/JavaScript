091e087d99d8194b81232cff84dd53348d668272
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 15:27:49 +0600

    shift() method removes the first array element shift other elements to lower index

---
+++ Array Method/shifting element/shifting.js
@@ -0,0 +1,6 @@
+//shift() method removes the first array element shift other elements to lower index
+const fruits = ["Mango", "Pineapple", "Apple", "Orange"];
+console.log("Before shifting: " + fruits);
+let fruit = fruits.shift();
+console.log("Shifted fruit: " + fruit);
+console.log("After shifting: " + fruits);
