cd68c7ccd2c93e9d6975246dc0ae46bdd0a9747d
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 15:09:38 +0600

    The push() method returns the new array length

--- Array Method/push method/pushMethod.js
+++ Array Method/push method/pushMethod.js
@@ -1,5 +1,6 @@
 //push() method adds a new element to an array to the end
 const bikes = ["Yamaha", "Bajaj", "Sujuki", "Pulsar"];
 console.log("Before adding: " + bikes);
-bikes.push("Royal Enfield");
+let length = bikes.push("Royal Enfield");
+console.log("Length= " + length);
 console.log("After adding: " + bikes);
