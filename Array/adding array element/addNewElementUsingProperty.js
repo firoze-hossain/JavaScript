bc8ac8e64bbdc25cf4f8b2dcb8951487fdaf5c57
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 07:55:04 +0600

    add new elememt to an array is used length property

---
+++ Array/adding array element/addNewElementUsingProperty.js
@@ -0,0 +1,5 @@
+//add new elememt to an array is used length property
+const fruits = ["Orange", "Banana", "Mango"];
+console.log("Before adding new element: " + fruits);
+fruits[fruits.length] = "Apple";
+console.log("After adding new element: " + fruits);
