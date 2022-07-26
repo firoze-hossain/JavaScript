ce022464ccdee96392a83499a0a42fb4318b3074
roze28 <firoze.hossain01@yahoo.com>
2022-07-26 09:04:20 +0600

    js array suport only numbered index

---
+++ Array/associative arrays/associativeArray.js
@@ -0,0 +1,7 @@
+//js array suport only numbered index
+const person = [];
+person[0] = "Md. Firoze Hossain";
+person[1] = "Java Developer";
+person[2] = 27;
+person[3] = "works at Leads";
+console.log(person[0] + " " + person[3]);
