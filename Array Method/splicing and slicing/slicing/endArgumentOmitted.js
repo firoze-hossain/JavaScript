d4907127f761395502f69b22e59e62ddb680c7eb
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 19:29:16 +0600

    if end argument is omitted slice() method slices out rest of the array

---
+++ Array Method/splicing and slicing/slicing/endArgumentOmitted.js
@@ -0,0 +1,5 @@
+//if end argument is omitted slice() method slices out rest of the array
+const fruits = ["Mango", "Guava", "Pineapple", "Lemon", "Apple"];
+console.log("Before sliced: " + fruits);
+let slicedFruits = fruits.slice(2);
+console.log("Sliced fruits: " + slicedFruits);
