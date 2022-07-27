7990bc8ae34c979adab68ceb3215f787ac6a2873
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 12:15:46 +0600

    js array length provides easy way to show new element

---
+++ Array Method/Array Length/arrayLength.js
@@ -0,0 +1,4 @@
+//js array length provides easy way to show new element
+const bikes = ["Yamaha", "Bajaj", "Sujuki"];
+bikes[bikes.length] = "Pulsar";
+console.log("After new element added: " + bikes);
