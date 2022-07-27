610d8fc547cda6d55c928585ff8b9ef9b7700337
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 22:58:04 +0600

    sort() method sorting array alphabetically

---
+++ Sorting Array/Sorting an array/sortingArray.js
@@ -0,0 +1,5 @@
+//sort() method sorting array alphabetically
+const fruits = ["Mango", "Pineapple", "Guava", "Apple", "Orange", "Lemon"];
+console.log("Before sorting: " + fruits);
+fruits.sort();
+console.log("After Sorting: " + fruits);
