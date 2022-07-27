b15d69396aa407be81a23c2a345f9bd9929d3524
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 19:49:56 +0600

    output with toString() method

---
+++ Array Method/automatic toString/example1.js
@@ -0,0 +1,4 @@
+//js automatically converts an array with comma separted string when expecting primitve value
+//output with toString() method
+const fruits = ["Mango", "Lemon", "Apple"];
+console.log(fruits.toString());
