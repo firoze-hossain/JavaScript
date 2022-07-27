4ab2343360c0de84fb22d57cac8af4d79fc0df2e
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 12:00:24 +0600

    unshift() method adds element in array at beginning and unshift older element

---
+++ Array Method/unshift/unshift.js
@@ -0,0 +1,6 @@
+//unshift() method adds element in array at beginning and unshift older element
+const fruits = ["Lemon", "Orange", "Apple", "Grape"];
+console.log("Before unshifting = " + fruits);
+let unshiftedFruit = fruits.unshift("Mango");
+console.log("Unshfited Fruit = " + unshiftedFruit);
+console.log("After unshifting: " + fruits);
