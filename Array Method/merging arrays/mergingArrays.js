ea3c002d387edb8515dde21e3c59f93ea4c860f3
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 17:19:58 +0600

    concat() method creates new array by merging existing arrays

---
+++ Array Method/merging arrays/mergingArrays.js
@@ -0,0 +1,5 @@
+//concat() method creates new array by merging existing arrays
+const boys = ["Firoze", "Millat", "Rayhan"];
+const girls = ["Sumaiya", "Mou", "Anamika"];
+let boysGirls = boys.concat(girls);
+console.log("Boys and girls together: " + boysGirls);
