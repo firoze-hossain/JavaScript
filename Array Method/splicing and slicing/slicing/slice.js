017a1bb6282647ab8cf0019e5b985155541d6c29
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 18:49:04 +0600

    slice() method slices out a piece array into new array

---
+++ Array Method/splicing and slicing/slicing/slice.js
@@ -0,0 +1,6 @@
+//slice() method slices out a piece array into new array
+//this example slice out part of an array starting from array element 1
+const fruits = ["Mango", "Apple", "Guava", "Pineapple"];
+console.log("Total Fruits: " + fruits);
+let sliceFruits = fruits.slice(1);
+console.log("Sliced Fruits: " + sliceFruits);
