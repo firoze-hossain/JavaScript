e5691ef9cc520bd7ee878924522a22ca967d03fb
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 19:20:14 +0600

    slice() method can take two argument

---
+++ Array Method/splicing and slicing/slicing/twoArgument.js
@@ -0,0 +1,6 @@
+//slice() method can take two argument
+//slice(x,y) starting from x index and finished before y index
+const fruits = ["Mango", "Apple", "Orange", "Guava", "Pineapple", "Lemon"];
+console.log("Before slicing: " + fruits);
+let slicedFruits = fruits.slice(2, 5);
+console.log("Sliced fruits list: " + slicedFruits);
