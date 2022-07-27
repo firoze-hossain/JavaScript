2d32361627c38ca4246948fc94c49eda4d0cf96b
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 23:07:03 +0600

    reverse() method reverse elements in an array

---
+++ Sorting Array/reverse method/reverseArray.js
@@ -0,0 +1,16 @@
+//reverse() method reverse elements in an array
+//we can use to sort element in descending order
+const fruits = [
+  "Lemon",
+  "Guava",
+  "Pineapple",
+  "Apple",
+  "Ornage",
+  "Banana",
+  "Grape",
+];
+console.log("Fruits list: " + fruits);
+fruits.sort();
+console.log("After sorting: " + fruits);
+fruits.reverse();
+console.log("After sorting element: " + fruits);
