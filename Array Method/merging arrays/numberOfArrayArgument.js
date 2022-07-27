2c2435aa2110b8b9513765b57e0e86a839bf1a24
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 17:29:19 +0600

    concat() method can take any number of array argument

---
+++ Array Method/merging arrays/numberOfArrayArgument.js
@@ -0,0 +1,6 @@
+//concat() method can take any number of array argument
+const javaDevlopers = ["Firoze", "Sohan", "Sonia"];
+const phpDevelopers = ["Millat", "Imtiaze", "Rony"];
+const pythonDevelopers = ["Abir", "Nijhum"];
+let allDevlopers = javaDevlopers.concat(phpDevelopers, pythonDevelopers);
+console.log("All software developers: " + allDevlopers);
