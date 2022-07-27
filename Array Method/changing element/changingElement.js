904ac09098d6dada5da3672b5eeb40b752c028ae
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 12:09:29 +0600

    changing element in array

---
+++ Array Method/changing element/changingElement.js
@@ -0,0 +1,5 @@
+//changing element in array
+const fruits = ["Mango", "Apple", "Guava", "Pineapple"];
+console.log("Before changing element: " + fruits);
+fruits[1] = "Orange";
+console.log("After changing element: " + fruits);
