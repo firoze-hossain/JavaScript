fed2527f20cadd28aa37221ad78262253a5f6358
roze28 <firoze.hossain01@yahoo.com>
2022-07-29 19:13:27 +0600

    forEach() method calls a function once for every array item

---
+++ Array Iteration/forEach Method/forEach.js
@@ -0,0 +1,9 @@
+//array iterates operates on every each item of array
+//forEach() method calls a function once for every array item
+const numbers = [2, 8, 25, 36, 1, 74];
+let txt = "";
+numbers.forEach(myFunction);
+console.log(txt);
+function myFunction(value) {
+  txt += value + "\n";
+}
