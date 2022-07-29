c6dd02b722262c261324bf45643d8ac0918c2f9a
roze28 <firoze.hossain01@yahoo.com>
2022-07-29 19:19:18 +0600

    updated

--- Array Iteration/forEach Method/forEach.js
+++ Array Iteration/forEach Method/forEach.js
@@ -4,6 +4,9 @@ const numbers = [2, 8, 25, 36, 1, 74];
 let txt = "";
 numbers.forEach(myFunction);
 console.log(txt);
+// function myFunction(value) {
+//   txt += value;
+// }
 function myFunction(value, index, array) {
   txt += value + "\n";
 }
