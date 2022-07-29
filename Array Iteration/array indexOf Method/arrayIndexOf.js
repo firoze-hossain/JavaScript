7be29431e99b83b963bb51f3f612650407bb4776
roze28 <firoze.hossain01@yahoo.com>
2022-07-29 23:46:35 +0600

    indexOf() method searches an array for an element value and return index of element

---
+++ Array Iteration/array indexOf Method/arrayIndexOf.js
@@ -0,0 +1,4 @@
+//indexOf() method searches an array for an element value and return index of element
+const fruits = ["Mango", "Apple", "Lemon", "Guava", "Pineapple"];
+let position = fruits.indexOf("Apple") + 1;
+console.log("Position of Apple: " + position);
