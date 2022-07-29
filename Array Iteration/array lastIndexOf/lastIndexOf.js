ca750066d9c9f33522000340cb32467567e178bf
roze28 <firoze.hossain01@yahoo.com>
2022-07-30 00:00:29 +0600

    returns the last occurrence of the specified element

---
+++ Array Iteration/array lastIndexOf/lastIndexOf.js
@@ -0,0 +1,4 @@
+//returns the last occurrence of the specified element
+const fruits = ["Mango", "Apple", "Guava", "Mango", "Pineapple"];
+let position = fruits.lastIndexOf("Guava") + 1;
+console.log("Apple is found on position: " + position);
