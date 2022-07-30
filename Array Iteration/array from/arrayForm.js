d771f0298e105fd7552b49cf8e9d61c52143dda8
roze28 <firoze.hossain01@yahoo.com>
2022-07-30 13:11:14 +0600

    Array.from() returns an array object from any object

---
+++ Array Iteration/array from/arrayForm.js
@@ -0,0 +1,3 @@
+//Array.from() returns an array object from any object with length property or any iterabble obejct
+const arrFrom = Array.from("ABCDEFGH");
+console.log("Array Obejct: " + arrFrom);
