bf01fc26badf907d070ae4682e760e87603ac940
roze28 <firoze.hossain01@yahoo.com>
2022-07-30 13:45:48 +0600

    array.includes() is used to check if an element is present in an array

---
+++ Array Iteration/array includes/arrayIncludes.js
@@ -0,0 +1,4 @@
+//array.includes() is used to check if an element is present in an array
+const fruits = ["Mango", "Apple", "Lemon", "Guava", "Pineapple"];
+let checkElement = fruits.includes("Apple");
+console.log("Apple is include in fruits list: " + checkElement);
