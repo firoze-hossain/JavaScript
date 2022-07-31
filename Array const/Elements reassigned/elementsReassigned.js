dd5256799f563becadc5b4a63892955df344f1ee
roze28 <firoze.hossain01@yahoo.com>
2022-07-31 16:19:52 +0600

    we can change the element of const array

---
+++ Array const/Elements reassigned/elementsReassigned.js
@@ -0,0 +1,6 @@
+//we can change the element of const array
+const cars = ["BMW", "Volvo", "Toyoto", "Yamaha", "Marceedes"];
+console.log("Before changing elements: " + cars);
+cars[1] = "Sujuki";
+cars.push();
+console.log("After changing elements: " + cars);
